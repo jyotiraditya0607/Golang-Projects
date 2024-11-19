8cd8f038e1c8a9b2ab0508d6c30f542dd0043c58
jyotiraditya0607 <jyotiradityabiswal64@gmail.com>
2024-11-19 14:51:56 +0530

    Refactor routing logic

--- go-movies-curd/main.go
+++ go-movies-curd/main.go
@@ -28,10 +28,32 @@ func getMovies(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(movies)
 }
 
-func getMovie(w http.ResponseWriter, r *http.Request)     {}
-func createMovie(w http.ResponseWriter, r *http.Request)  {}
-func updateMovie(w http.ResponseWriter, r *http.Request)  {}
-func deleteMovies(w http.ResponseWriter, r *http.Request) {}
+func deleteMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
+}
+
+func getMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for _, item := range movies {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+}
+func createMovie(w http.ResponseWriter, r *http.Request) {}
+func updateMovie(w http.ResponseWriter, r *http.Request) {}
 
 func main() {
 	r := mux.NewRouter()
